d6b16fa29307a33df8fd1816c39b082bd2ef8e51
Björn Åhström <ahstrom_81@hotmail.com>
2019-08-23 10:38:03 +0200

    Fixed fireman positions

---
+++ Assets/Scripts/FiremanController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiremanController : MonoBehaviour
+{
+    public List<Transform> positions = new List<Transform>();
+
+
+    private void Start()
+    {
+        transform.position = positions[1].position;
+    }
+
+}
